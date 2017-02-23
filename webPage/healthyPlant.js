9f98fbf4c780333ea27a0be4bd8265f7ee8f865a
Weston Clark <clark.westonp@gmail.com>
2017-02-23 13:45:30 -0700

    added the html external js file to process the updates from the server

---
+++ webPage/healthyPlant.js
@@ -0,0 +1,26 @@
+/*****************************************************
+* This function will route the element to be 
+* updated based off the id passed in from the server
+*****************************************************/
+function router(id){
+    
+}
+
+
+
+
+function temprature(){
+    
+}
+
+function humidity(){
+    
+}
+
+function light(){
+    
+}
+
+function moisture(){
+    
+}

a04efe1efa13fa841828ac3ef8446c950a4dbeb0
jmefford <mefford.ja@gmail.com>
2017-03-19 23:04:14 -0600

    Updated the database to have proper inserts for the plants and so that it was in Mysql

--- Database/plants.sql
+++ Database/plants.sql
@@ -1,68 +1,136 @@
+/****** Drop the tables if they already exist within the database ******/
+DROP TABLE temp;
+DROP TABLE light;
+DROP TABLE moist;
+DROP TABLE humid;
 DROP TABLE plants;
 DROP TABLE readings;
 
+/****** Create the tables that are needed for the database ******/
 CREATE TABLE plants { -- add the two extreems lowest and highest
-   id         SERIAL PRIMARY KEY
-,  name       TEXT NOT NULL
-,  sci_name   TEXT
-,  temp_id    INT  REFERENCES temp(id)
-,  humid_id   INT  REFERENCES humid
-,  light_id   INT  REFERENCES light(id)
-,  moist_id   INT  REFERENCES moist(id)
+   id         INT UNSIGNED PRIMARY KEY AUTO_INCREMENT
+,  name       CHAR(20) NOT NULL
+,  sci_name   CHAR(255)
+,  temp_id    INT
+,  humid_id   INT
+,  light_id   INT
+,  moist_id   INT
 };
 
 CREATE TABLE readings {
-   id        SERIAL PRIMARY KEY
-,  plant_id  INT REFERENCES plants(id)
+   id        INT UNSIGNED PRIMARY KEY AUTO_INCREMENT
+,  plant_id  INT
 ,  temp      INT
 ,  humidity  INT
 ,  light     INT
 ,  moisture  INT
 ,  last_read DATE
+,  CONSTRAINT redings_fk1 FOREIGN KEY (plant_id) REFERENCES plants (id)
 };
 
 CREATE TABLE temp {
-   id            SERIAL PRIMARY KEY
+   id            INT UNSIGNED PRIMARY KEY AUTO_INCREMENT
 ,  highest_temp  INT
 ,  best_temp     INT
 ,  lowest_temp   INT
 }
 
 CREATE TABLE light {
-   id     SERIAL PRIMARY KEY
+   id           INT UNSIGNED PRIMARY KEY AUTO_INCREMENT
 ,  high_light   INT
 ,  best_light   INT
 ,  lowest_light INT
 }
 
 CREATE TABLE moist {
-   id     SERIAL PRIMARY KEY
+   id           INT UNSIGNED PRIMARY KEY AUTO_INCREMENT
 ,  high_moist   INT
 ,  best_moist   INT
 ,  lowest_moist INT
 }
 
-INSERT INTO plants 
-(name, sci_name, best_temp, best_humid, best_light, best_moist)
+CREATE TABLE humid {
+   id           INT UNSIGNED PRIMARY KEY AUTO_INCREMENT
+,  high_humid   INT
+,  best_humid   INT
+,  lowest_humid INT
+}
+
+/****** Add Foreign Key constr ******/
+ALTER TABLE plants
+   ADD CONSTRAINT plant_fk1 FOREIGN KEY (temp_id) REFERENCES temp (id);
+
+ALTER TABLE plants
+   ADD CONSTRAINT plant_fk2 FOREIGN KEY (humid_id) REFERENCES humid (id);
+
+ALTER TABLE plants
+   ADD CONSTRAINT plant_fk3 FOREIGN KEY (light_id) REFERENCES light (id);
+
+ALTER TABLE plants
+   ADD CONSTRAINT plant_fk4 FOREIGN KEY (moist_id) REFERENCES moist (id);
+   
+/****** Insert into the tables ******/   
+INSERT INTO temp
+(high_temp, best_temp, lowest_temp)
 VALUES
-("Zebra Cactus", "Haworthia", "N/A", "Not Direct Sunlight or Deep Shade", "N/A");
+(100,70,40);
+
+INSERT INTO light
+(high_light, best_light, lowest_light)
+VALUES
+(100,70,20);
+
+INSERT INTO light
+(high_light, best_light, lowest_light)
+VALUES
+(90,70,20);
+
+INSERT INTO moist
+(high_moist, best_moist, lowest_moist)
+VALUES
+(80,50,30);
+
+INSERT INTO moist
+(high_moist, best_moist, lowest_moist)
+VALUES
+(70,40,10);
+
+INSERT INTO humid
+(high_humid, best_humid, lowest_humid)
+VALUES
+(100,70,40);
+
+INSERT INTO humid
+(high_humid, best_humid, lowest_humid)
+VALUES
+(90,70,40);
+
+INSERT INTO humid
+(high_humid, best_humid, lowest_humid)
+VALUES
+(70,40,10);
 
 INSERT INTO plants 
-(name, sci_name, best_temp, best_humid, best_light, best_moist)
+(name, sci_name, temp_id,  humid_id,  light_id,  moist_id)
 VALUES
-("Aloe Vera", "Aloe Barbadensis", "N/A", "N/A", "Not Direct Sunlight or Deep Shade", "N/A");
+("Zebra Cactus", "Haworthia", 1, 2, 1, 2);
 
 INSERT INTO plants 
-(name, sci_name, best_temp, best_humid, best_light, best_moist)
+(name, sci_name, temp_id,  humid_id,  light_id,  moist_id)
 VALUES
-("Cactus", "", "N/A", "N/A", "Not Deep Shade", "N/A");
+("Aloe Vera", "Aloe Barbadensis",  1, 2, 1, 2);
 
 INSERT INTO plants 
-(name, sci_name, best_temp, best_humid, best_light, best_moist)
+(name, sci_name, temp_id,  humid_id,  light_id,  moist_id)
 VALUES
-("Rosularia", "Crassulaceae Rosularia", "N/A", "Not Direct Sunlight or Deep Shade", "N/A");
+("Cactus", "",  1, 2, 1, 2);
 
 INSERT INTO plants 
-(name, sci_name, best_temp, best_humid, best_light, best_moist)
+(name, sci_name, temp_id,  humid_id,  light_id,  moist_id)
 VALUES
-("Peace Lily", "Spathiphyllum", "N/A", "Not Direct Sunlight", "N/A");
+("Rosularia", "Crassulaceae Rosularia",  1, 2, 1, 2);
+
+INSERT INTO plants 
+(name, sci_name, temp_id,  humid_id,  light_id,  moist_id)
+VALUES
+("Peace Lily", "Spathiphyllum",  1, 1, 1, 1);

9222c0fa885c43df27992d08492d0f590169ae91
jmefford <mefford.ja@gmail.com>
2017-04-05 00:42:55 -0600

    little fix

--- Database/insert.php
+++ Database/insert.php
@@ -1,21 +1,19 @@
 <?php include 'dbaccess.php';?>
 <?php
 
-    if(isset($_POST['read'])){
+    if(isset($_POST['temp'])){
 		//echo "We made it submit!";
 		$plant = isset($_POST['plant']) ? $_POST['plant'] : '';
 		$temp  = isset($_POST['temp'])  ? $_POST['temp']  : ''; 
 		$humid = isset($_POST['humid']) ? $_POST['humid'] : ''; 
 		$moist = isset($_POST['moist']) ? $_POST['moist'] : '';
-		$date  = isset($_POST['date'])  ? $_POST['date']  : '';
-	}
+    }
 	else{
 		//echo "didn't submit";
 		$plant = '';
 		$temp  = '';
 		$humid = '';
 		$moist = '';
-		$date  = '';
 	}
 
     if(isset($_POST['read'])){
@@ -23,12 +21,11 @@
         $plant = $_POST
         
 		//echo "did an insert!";
-		$stmt = $db->prepare("INSERT INTO readings(plant_id, temp, humid, moist, last_read) VALUES(:plant, :temp, :humid, :moist, :light, :date)");
+		$stmt = $db->prepare("INSERT INTO readings(plant_id, temp, humid, moist) VALUES(:plant, :temp, :humid, :moist, :light");
         $stmt->bindParam(":plant", $plant, PDO :: PARAM_INT);
 		$stmt->bindParam(":temp", $temp, PDO::PARAM_IN);
 		$stmt->bindParam(":humid", $humid, PDO::PARAM_INT);
 		$stmt->bindParam(":moist", $moist, PDO::PARAM_INT);
-        $stmt->bindParam(":date", $date, PDO::PARAM_INT);
 		$stmt->execute();
 		
 	}
